816d78592261615e467813e33a87dc13706edcca
Simon <simon@coppelia.io>
2023-08-17 16:54:48 +0100

    Misc edits

--- pybarb/pybarb.py
+++ pybarb/pybarb.py
@@ -612,8 +612,8 @@ class ProgrammeRatingsResultSet(APIResultSet):
             episode_name = None
             episode_number = None
             genre = None
-            if e['programme_content'] is not None
-                prog_name = e['programme_content']['programme_name'].title()
+            if e['programme_content'] is not None:
+                prog_name = e['programme_content']['content_name']
                 if 'episode' in e['programme_content'].keys():
                     episode_name = e['programme_content']['episode']['episode_name']
                     episode_number = e['programme_content']['episode']['episode_number']
